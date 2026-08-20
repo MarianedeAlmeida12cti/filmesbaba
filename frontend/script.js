80c38bf887f41413a6c50aaadf02d267380d176b
EA - Mariane De Almeida Mendes <mariane_mendes@educadventista.org>
2026-08-20 08:20:03 -0300

    iu

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 async function buscarFilmes() {
     try {
-        const resposta = await fetch("https://sua-api.com/MarianedeAlmeida12cti/filmesbaba");
+        const resposta = await fetch("https://filmesbaba-mx34.vercel.app/");
 
         if (!resposta.ok) {
             throw new Error(`Erro na requisição: ${resposta.status}`);
@@ -16,7 +16,7 @@ async function buscarFilmes() {
 
         sectionFilmes.innerHTML = filmes.map((filme) => `
             <div>
-                <h2>${filme.nome || filme.name}</h2>
+                <h2>${filme.name}</h2>
                 <p><strong>Gênero:</strong> ${filme.genero}</p>
                 <p><strong>Duração:</strong> ${filme.duracao} minutos</p>
                 <p><strong>Classificação indicativa:</strong> ${
